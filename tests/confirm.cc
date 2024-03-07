c2de930d3f6bb0e125ff82dc64155c0f0a4a8939
Donald Lee <donaldturinglee@gmail.com>
2024-03-07 01:30:34 -0500

    fix: Change the name of the is_passing_grade function to is_negative, as it makes more sense

--- tests/confirm.cc
+++ tests/confirm.cc
@@ -1,6 +1,6 @@
 #include "ccunit.h"
 
-bool is_passing_grade(int value) {
+bool is_negative(int value) {
 	if(value < 60) {
 		return false;
 	}
@@ -12,8 +12,8 @@ TEST("Test will pass without any confirms") {
 }
 
 TEST("Test passing grades") {
-	bool result = is_passing_grade(0);
+	bool result = is_negative(0);
 	CONFIRM_FALSE(result);
-	result = is_passing_grade(100);
+	result = is_negative(100);
 	CONFIRM_TRUE(result);
 }
