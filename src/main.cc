dbff293edafbdf4ba4cc9ab2c1d954160f699d81
Donald Lee <donaldturinglee@gmail.com>
2024-06-28 11:08:43 +0800

    feat: Create a basic test function

---
+++ src/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+void test_can_be_created() {
+    std::cout << "Test can be created." << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+    test_can_be_created();
+    return 0;
+}
